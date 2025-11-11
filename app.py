4192a4e148d2fc7249c4daa68495af8b75b53d9b
Prari2725 <pragatirpehrkar@gmail.com>
2025-11-11 12:04:31 +0530

    Fix flake8 linting issues

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 def add(a, b):
     return a + b
 
+
 if __name__ == "__main__":
     print("Sum of 2 and 3 is:", add(2, 3))
+    print("Sum of -1 and 1 is:", add(-1, 1))
+    print("Sum of 0 and 0 is:", add(0, 0))
+    print("Sum of -2 and -3 is:", add(-2, -3))
+
+
+
--- test_app.py
+++ test_app.py
@@ -1,7 +1,5 @@
 import app
 
+
 def test_add():
     assert app.add(2, 3) == 5
-    assert app.add(-1, 1) == 0
-    assert app.add(0, 0) == 0       
-    assert app.add(-2, -3) == -5
